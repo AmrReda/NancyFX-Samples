28dc9685133d1b464f09013b15337bbfdb5ec356
Amr Reda <amrreda@outlook.com>
2015-09-01 22:10:58 +1000

    NancyFX.Hosting.AspNet project added

---
+++ NancyFX.Hosting.AspNet/NancyFX.Hosting.AspNet/HomeModule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy;
+
+namespace NancyFX.Hosting.AspNet
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = _ => "Hello Nancy from Amr Reda";
+            Get["/home"] = _ => View["home"];
+        }
+    }
+}
