0b73dfffa46986ca064b4379c66178791ce95dcb
Amr Reda <amrreda@outlook.com>
2015-09-01 22:49:30 +1000

    Boostraper added to solve the root path issue

---
+++ NancyFX.Hosting.Wcf/NancyFX.Hosting.Wcf/Bootstraper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy;
+using Nancy.Hosting.Wcf;
+
+namespace NancyFX.Hosting.Wcf
+{
+    public class Bootstraper : DefaultNancyBootstrapper
+    {
+        protected override IRootPathProvider RootPathProvider
+        {
+            get { return new ServiceRootProvider(); }
+        }
+    }
+
+    public class ServiceRootProvider : IRootPathProvider
+    {
+        private IRootPathProvider provider = new FileSystemRootPathProvider();
+
+        public string GetRootPath()
+        {
+            return Path.GetFullPath(Path.Combine(provider.GetRootPath(), "..", ".."));
+        }
+    }
+}
